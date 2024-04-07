2de2a13257efdfdccb39440c27b07acbf2fc7f00
maria <masha.masharskaya@gmail.com>
2024-04-07 19:04:56 +0400

    change connection

--- tests11.py
+++ tests11.py
@@ -2,7 +2,7 @@ import pymssql
 import pytest
 
 MSSQL_HOST = '127.0.0.1'
-MSSQL_PORT = '1433'
+MSSQL_PORT = 1433
 MSSQL_USER = 'testUser'
 MSSQL_PASSWORD = '123456!M654321'
 MSSQL_DATABASE = 'TRN'
@@ -10,7 +10,7 @@ MSSQL_DATABASE = 'TRN'
 # Fixture for the database connection
 @pytest.fixture(scope="function")
 def db_connection():
-    connection = pymssql.connect(server=MSSQL_HOST, user=MSSQL_USER, password=MSSQL_PASSWORD, database=MSSQL_DATABASE, port=MSSQL_PORT)
+    connection = pymssql.connect(host=MSSQL_HOST, user=MSSQL_USER, password=MSSQL_PASSWORD, database=MSSQL_DATABASE, port=MSSQL_PORT)
     try:
         yield connection
     finally:
@@ -36,35 +36,28 @@ def test_null_values(db_connection):
 
 def test_duplicates(db_connection):
     cursor = db_connection.cursor()
-    cursor.execute("WITH cte AS (SELECT job_id, count(*) as cnt FROM hr.jobs "
-                                           "GROUP BY job_id HAVING count(*)>1) "
-                                           "SELECT CASE WHEN count(*)=0 THEN 1 ELSE 0 END AS res FROM cte")
+    cursor.execute("WITH cte AS (SELECT job_id, count(*) as cnt FROM hr.jobs GROUP BY job_id HAVING count(*)>1) SELECT CASE WHEN count(*)=0 THEN 1 ELSE 0 END AS res FROM cte")
     result = cursor.fetchone()
     assert result.scalar() == 1, "Should return 1 for hr.jobs table if there is no duplicates"
 
 
 def test_min_max_salary_comparison(db_connection):
     cursor = db_connection.cursor()
-    cursor.execute("WITH cte AS (SELECT * FROM hr.jobs WHERE min_salary>max_salary) "
-                                           "SELECT CASE WHEN count(*)=0 THEN 1 ELSE 0 END AS res FROM cte")
+    cursor.execute("WITH cte AS (SELECT * FROM hr.jobs WHERE min_salary>max_salary) SELECT CASE WHEN count(*)=0 THEN 1 ELSE 0 END AS res FROM cte")
     result = cursor.fetchone()
     assert result.scalar() == 1, "Should return 1 for hr.jobs table if min salary is not more than maximal salary"
 
 
 def test_email(db_connection):
     cursor = db_connection.cursor()
-    cursor.execute("with cte as (SELECT * FROM hr.employees WHERE email NOT LIKE '%__@__%.__%') "
-                                           "SELECT CASE WHEN count(*)=0 THEN 1 ELSE 0 END AS res FROM cte")
+    cursor.execute("with cte as (SELECT * FROM hr.employees WHERE email NOT LIKE '%__@__%.__%') SELECT CASE WHEN count(*)=0 THEN 1 ELSE 0 END AS res FROM cte")
     result = cursor.fetchone()
     assert result.scalar() == 1, "Should return 1 if email field follows specified format in hr.employees table"
 
 
 def test_main_manager_salary(db_connection):
     cursor = db_connection.cursor()
-    cursor.execute("WITH cte AS (SELECT * FROM hr.employees "
-                                           "WHERE manager_id is null and salary = "
-                                           "(SELECT MAX(salary) FROM hr.employees)) "
-                                           "SELECT CASE WHEN COUNT(*)=0 THEN 0 ELSE 1 END AS res FROM cte")
+    cursor.execute("WITH cte AS (SELECT * FROM hr.employees WHERE manager_id is null and salary = (SELECT MAX(salary) FROM hr.employees)) SELECT CASE WHEN COUNT(*)=0 THEN 0 ELSE 1 END AS res FROM cte")
     result = cursor.fetchone()
     assert result.scalar() == 1, "Should return 1 if main manager has the biggest salary in hr.employees table"
 
